7210c71be1676860c63898d3cc8c0710ada94d94
Steven Yi <stevenyi@gmail.com>
2017-10-31 18:31:59 -0400

    trying to work out loading wasm binary

--- CsoundProcessor.js
+++ CsoundProcessor.js
@@ -2,13 +2,20 @@
  * 
  */
 
-'use strict';
+//'use strict';
 
 class CsoundProcessor extends AudioWorkletProcessor {
 
   constructor(options) {
     super(options);
 
+
+    this.port.onmessage = evt => {
+
+    };
+    this.port.start();
+
+    this.port.postMessage(options);
   }
 
   process(inputs, outputs, parameters) {
@@ -17,13 +24,17 @@ class CsoundProcessor extends AudioWorkletProcessor {
   } 
 }
 
+var importObject = { imports: { print: arg => console.log(arg) } };
 
 // Load WASM
-fetch('libcsound.wasm').then(response =>
-  response.arrayBuffer()
-).then(bytes =>
-  WebAssembly.instantiate(bytes, importObject)
-).then(result =>
-  registerProcessor('Csound', CsoundProcessor)
-    //result.instance.exports.exported_func()
-);
+//fetch('libcsound.wasm').then(response =>
+//  response.arrayBuffer()
+//).then(bytes =>
+//  WebAssembly.instantiate(bytes, importObject)
+//).then(result =>
+//  //registerProcessor('Csound', CsoundProcessor)
+//    //result.instance.exports.exported_func()
+//);
+
+
+  registerProcessor('Csound', CsoundProcessor);
